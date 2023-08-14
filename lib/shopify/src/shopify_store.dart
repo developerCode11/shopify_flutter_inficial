71f417edc4a5abfd652956d5c07be952bb7eb88d
Kishan Busa <kishanbusa08@gmail.com>
2023-08-14 15:05:19 +0530

    - fixes

--- lib/shopify/src/shopify_store.dart
+++ lib/shopify/src/shopify_store.dart
@@ -742,6 +742,7 @@ class ShopifyStore with ShopifyError {
       int index = stagedUpload.stagedUploadsCreate?.stagedTargets
               ?.indexWhere((element) => element.url == stagedTarget.url) ??
           -1;
+      print(stagedTarget.resourceUrl);
       await uploadMediaToTheShopifyStore(
           stagedTargets: stagedTarget,
           file: File(input.input?[index].filepath ?? ''));
